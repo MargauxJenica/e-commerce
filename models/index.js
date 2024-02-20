e1e19fb014b692b75b0dc8ea1c182e2be500a987
Margaux <margaux.santos113@gmail.com>
2024-02-20 13:00:14 -0800

    columns in models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  hooks: true
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }, 
+    as: 'product_tags'
+  });
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+    as:'group_tags'
+  });
 module.exports = {
   Product,
   Category,
